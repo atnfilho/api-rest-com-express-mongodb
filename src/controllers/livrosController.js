6e33dd952e56dd3953a839ed462f82ead24408a9
Antonio da Silva Santos Filho <antonio.filho@bb.com.br>
2022-08-29 19:45:20 -0300

    Aula 05: Aprimorando o projeto.

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -3,19 +3,23 @@ import livros from "../models/Livro.js";
 class LivroController {
 
     static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
-            res.status(200).json(livros);
-        });
+        livros.find()
+            .populate('autor')
+            .exec((err, livros) => {
+                res.status(200).json(livros);
+            });
     }
 
     static listarLivroPorId = (req, res) => {
         const id = req.params.id;
-        livros.findById(id, (err, livros) => {
-            if(err) {
-                res.status(400).send({message: `${err.message} - Id do livro não localizado.`});
-            }
-            res.status(200).send(livros);
-        });
+        livros.findById(id)
+            .populate('autor', 'nome')
+            .exec((err, livros) => {
+                if (err) {
+                    res.status(400).send({ message: `${err.message} - Id do livro não localizado.` });
+                }
+                res.status(200).send(livros);
+            });
     }
 
     static cadastrarLivro = (req, res) => {
@@ -41,10 +45,18 @@ class LivroController {
     static excluirLivro = (req, res) => {
         const id = req.params.id;
         livros.findByIdAndDelete(id, (err) => {
-            if(err) {
-                res.status(500).send({message: `${err.message} - Falha ao excluir o livro.`});
+            if (err) {
+                res.status(500).send({ message: `${err.message} - Falha ao excluir o livro.` });
             }
-            res.status(200).send({message: 'Livro removido com sucesso.'});
+            res.status(200).send({ message: 'Livro removido com sucesso.' });
+        })
+    }
+
+    static listarLivroPorEditora = (req, res) => {
+        const editora = req.query.editora;
+
+        livros.find({ 'editora': editora }, {}, (err, livros) => {
+            res.status(200).send(livros);
         })
     }
 }
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -3,8 +3,10 @@ import LivroController from "../controllers/livrosController.js";
 
 const router = express.Router();
 
+// as rotas devem ser ordenadas da mais específica para a menos específica 
 router
     .get('/livros', LivroController.listarLivros)
+    .get('/livros/busca', LivroController.listarLivroPorEditora)
     .get('/livros/:id', LivroController.listarLivroPorId)
     .post('/livros', LivroController.cadastrarLivro)
     .put('/livros/:id', LivroController.atualizarLivro)
